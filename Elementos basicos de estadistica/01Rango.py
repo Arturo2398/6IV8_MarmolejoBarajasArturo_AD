1274ff608a7b94008b78bf1854a1d0cf501a252b
Arturo2398 <marmolejo.barajas.arturo@gmail.com>
2025-03-10 15:26:05 -0600

    rango.py

---
+++ Elementos basicos de estadistica/01Rango.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+##escribir un programa que le pregunte al usuario por las ventas de un rango de años y muestre en la pantalla una serie de datos de ventas indexadas 
+##por los años antes y despues de aplicar un descuento 
+
+inicio = int(input("Itroduce el año de ventas inicial: "))
+
+fin = int(input("Introduce eñ año final de ventas"))
+
+ventas = {}
+
+for i in range(inicio,fin+1):
+    ventas[i] = float(input("Introduce las ventas del año " + str(i) +':'))
+    
+
+ventas = pd.Series(ventas)
+
+print('Ventas\n ', ventas)
+print('Ventas con decuento\n', ventas*0.9)
