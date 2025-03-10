37b3826ef40dbfcbe405be25bae9c3f58ab35c05
Arturo2398 <marmolejo.barajas.arturo@gmail.com>
2025-03-10 15:44:30 -0600

    cambio por aprobados

--- Elementos basicos de estadistica/02Diccionaro.py
+++ Elementos basicos de estadistica/02Diccionaro.py
@@ -7,7 +7,12 @@ def estadistica_notas(notas):
     estadisticas = pd.Series([notas.min(), notas.max(),notas.mean(),notas.std()], 
                              index=['Min', 'Max','Media','Desviacion'])
     return estadisticas
-notas = {'Juan': 9, 'Juanita': 7, 'Pedro': 6.6,'Fabian':8.5,'Maximiliano':7.5,
+
+def aprobados(notas):
+    notas = pd.Series(notas)
+    return notas[notas>=6].sort_values(ascending = False)
+notas = {'Juan': 5.9, 'Juanita': 5, 'Pedro': 6.6,'Fabian':8.5,'Maximiliano':7.5,
          'Sandra': 9.8 ,'Rosario': 9}
 
 print(estadistica_notas(notas))
+print(aprobados(notas))
