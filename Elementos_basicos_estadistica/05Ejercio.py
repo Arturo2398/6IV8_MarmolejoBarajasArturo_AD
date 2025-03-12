f1c3af9ab1b318bac5bc0c621728ac7697d7fee2
Arturo2398 <marmolejo.barajas.arturo@gmail.com>
2025-03-12 00:01:30 -0600

    ejercicio estadistica

--- Elementos_basicos_estadistica/05Ejercio.py
+++ Elementos_basicos_estadistica/05Ejercio.py
@@ -2,10 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Cargar datos
+
 df = pd.read_csv('Elementos_basicos_estadistica/housing.csv')
 
-# Cálculo de estadísticas descriptivas
 estadisticas = df["median_house_value"].agg(["mean", "median", "var", "std"]).to_frame()
 
 estadisticas.loc["Moda"] = df["median_house_value"].mode().values[0]
@@ -16,7 +15,6 @@ estadisticas.index = ["Media", "Mediana", "Varianza", "Desviación Estándar", "
 estadisticas_df = estadisticas.reset_index()
 estadisticas_df.columns = ["Estadística", "Valor"]
 
-# Creación de tabla de frecuencias
 limites_clases = range(int(df["median_house_value"].min()), int(df["median_house_value"].max()) + 50000, 50000)
 df["Intervalo"] = pd.cut(df["median_house_value"], bins=limites_clases, right=False)
 
@@ -24,13 +22,11 @@ tabla_frecuencias = df["Intervalo"].value_counts().reset_index()
 tabla_frecuencias.columns = ["Intervalo de Valores", "Frecuencia"]
 tabla_frecuencias = tabla_frecuencias.sort_values(by="Intervalo de Valores")
 
-# Imprimir resultados
 print("\n================ ESTADÍSTICAS DESCRIPTIVAS ================".center(80))
 print(estadisticas_df.to_string(index=False))
 print("\n================= TABLA DE FRECUENCIAS =================".center(80))
 print(tabla_frecuencias.to_string(index=False))
 
-# Generar histogramas
 df[["median_house_value", "total_bedrooms", "population"]].hist(bins=30, alpha=0.5, figsize=(10, 6))
 plt.suptitle("Histograma Comparativo: Valor Medio de Vivienda vs Total de Dormitorios y Población")
 plt.show()
